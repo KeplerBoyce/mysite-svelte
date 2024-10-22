5a767e179114ddb02dd759a2da89cd3071fa7106
KeplerBoyce <keplerboy@gmail.com>
2024-10-22 02:11:14 -0400

    small edits to mysite tile

--- src/lib/components/tiles/ThisSite.svelte
+++ src/lib/components/tiles/ThisSite.svelte
@@ -28,7 +28,7 @@
   </div>
 
   <p slot="text">
-    This website, where I showcase all of my projects! I originally made this with Next.js, but I wanted to try Svelte to learn something new. So far, I'm really enjoying the templating system (slots are really nice) and simpler-to-use props and state compared to React's props and hooks.
+    This website, where I showcase all of my projects! I originally made this with Next.js, but I wanted to try Svelte to learn something new. So far, I'm really enjoying the templating system and simpler-to-use props and state compared to React's props and hooks.
   </p>
 
   <div slot="after" class="tags">
@@ -36,5 +36,6 @@
     <Tag text="Svelte" />
     <Tag text="Typescript" />
     <Tag text="TailwindCSS" />
+    <Tag text="CSS" />
   </div>
 </ImageBlock>
