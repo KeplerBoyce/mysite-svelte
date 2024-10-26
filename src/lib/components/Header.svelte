9599b7f5047bcc13076de46cde56c82b34d2c672
KeplerBoyce <keplerboy@gmail.com>
2024-10-26 02:26:49 -0400

    fix page height and scrolling behavior

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -3,7 +3,7 @@
   import Switch from "./Switch.svelte";
 </script>
 
-<div class="w-full h-16 flex gap-4 px-8 justify-center items-center border-b-2 border-gray-300 border-util">
+<div class="flex-none w-full h-16 flex gap-4 px-8 justify-center items-center border-b-2 border-gray-300 border-util">
   <a href="/" class="no-underline">
     <h1 class="text-left text-2xl">
       Kepler Boyce
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,9 +12,9 @@
   import { dark } from "$lib/stores";
 </script>
 
-<div class={`transition-colors duration-300 ${$dark ? "dark" : ""}`}>
+<div class={`h-full flex flex-col transition-colors duration-300 ${$dark ? "dark" : ""}`}>
   <Header />
-  <div class="p-8 pt-24 pb-16 flex flex-col items-center">
+  <div class="grow overflow-y-scroll p-8 pt-24 pb-16 flex flex-col items-center">
     <slot />
   </div>
 </div>
